843e4a8c9f27aa472765728bcb917ac0e0f8390e
NiccKurangTidur <desheamaulana273@gmail.com>
2023-04-27 11:36:42 +0700

    N

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <Hero/>
     
     </div>
   );
