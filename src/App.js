c871873fdbcadd3720a964b8e4303fcc24f4cce8
Dasterente <airadesta05@gmail.com>
2023-05-04 08:12:08 +0700

    Desta Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import Search from './components/Search';
+
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Search />
     
     </div>
   );
