cd491ddaf95131ceb73fa068af9526994915a343
NiccKurangTidur <desheamaulana273@gmail.com>
2023-05-16 14:07:20 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -5,18 +5,10 @@ import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Search from './components/Search';
-import SignIn from './components/signin';
-import SignUp from './components/signup';
 
 function App() {
   return (
     <div>
-      <div className="flex justify-center items-center h-screen">
-      <div className="w-1/3 bg-gray-100 p-8 rounded-lg shadow-lg">
-      <SignIn />
-      <hr className="my-8" />
-      </div>
-      </div>
       <Navbar />
       <Hero/>
       <Destinations/>
